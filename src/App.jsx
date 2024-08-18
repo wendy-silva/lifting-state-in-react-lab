290d2e0b1e23ef5b86f2135342fd2f3616b68906
wendy-silva <silvawendy0131@gmail.com>
2024-08-18 18:26:55 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+// src/App.jsx
+
+import './App.css';
+
+export const availableIngredients = [
+  { name: 'Kaiser Bun', color: 'saddlebrown' },
+  { name: 'Sesame Bun', color: 'sandybrown' },
+  { name: 'Gluten Free Bun', color: 'peru' },
+  { name: 'Lettuce Wrap', color: 'olivedrab' },
+  { name: 'Beef Patty', color: '#3F250B' },
+  { name: 'Soy Patty', color: '#3F250B' },
+  { name: 'Black Bean Patty', color: '#3F250B' },
+  { name: 'Chicken Patty', color: 'burlywood' },
+  { name: 'Lettuce', color: 'lawngreen' },
+  { name: 'Tomato', color: 'tomato' },
+  { name: 'Bacon', color: 'maroon' },
+  { name: 'Onion', color: 'lightyellow' },
+  { name: 'Cheddar Cheese', color: '#FDE18B' },
+  { name: 'Swiss Cheese', color: '#F1E1A8' },
+];
+
+const App = () => {
+  return (
+    <main>
+      <h1>Burger Stacker</h1>
+      <section>
+      {/* List & Stack components */}
+      </section>
+    </main>
+  );
+};
+
+export default App;
